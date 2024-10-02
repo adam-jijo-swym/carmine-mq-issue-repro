953418d76022fe2a26dc846453b697efc172aeac
Adam Jijo <158173116+adam-jijo-swym@users.noreply.github.com>
2024-10-02 22:10:04 +0530

    use carmine v3.2.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0",
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"] [com.taoensso/carmine "3.4.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"] [com.taoensso/carmine "3.2.0"]]
   :main ^:skip-aot carmine-mq-issue-repro.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all,
